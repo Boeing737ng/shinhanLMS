1a677bf96b474e139de9e42381ebd0cb38ec4903
hs9991 <hyunsoo99912@naver.com>
2019-08-22 16:56:49 +0900

    update group make

--- userApp/userApp/GroupBoardViewController.swift
+++ userApp/userApp/GroupBoardViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class GroupBoardViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
---
+++ userApp/userApp/GroupCreate2ViewController.swift
@@ -0,0 +1,134 @@
+//
+//  GroupCreate2ViewController.swift
+//  userApp
+//
+//  Created by user on 21/08/2019.
+//  Copyright © 2019 sfo. All rights reserved.
+//
+
+import UIKit
+
+class GroupCreate2ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    
+    let picker = UIImagePickerController()
+    
+    @IBOutlet weak var studyImg: UIImageView!
+    @IBOutlet weak var studyName: UITextField!
+    @IBOutlet weak var studyDetail: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        picker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+    func openLibrary(){
+        
+        picker.sourceType = .photoLibrary
+        present(picker, animated: false, completion: nil)
+        
+    }
+    
+//사진 이미지를 출력
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            studyImg.image = image
+            awakeFromNib()
+            print(info)
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func openCamera(){
+        if(UIImagePickerController .isSourceTypeAvailable(.camera)){
+            
+            picker.sourceType = .camera
+            present(picker, animated: false, completion: nil)
+            
+        }
+        
+        picker.sourceType = .camera
+        present(picker, animated: false, completion: nil)
+        
+    }
+    
+    @IBAction func studyAdd(_ sender: UIButton) {
+        let alert =  UIAlertController(title: "사진 위치", message: "선택", preferredStyle: .actionSheet)
+        
+        let library =  UIAlertAction(title: "사진앨범", style: .default) { (action) in self.openLibrary()
+        }
+        
+        let camera =  UIAlertAction(title: "카메라", style: .default) { (action) in self.openCamera()
+            
+        }
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(library)
+        alert.addAction(camera)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        let mediaType = info[UIImagePickerController
+//
+//    }
+    
+    @IBAction func onGoBack(_ sender: UIBarButtonItem) {
+        let transition: CATransition = CATransition()
+        transition.duration = 0.5
+        transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        transition.type = CATransitionType.reveal
+        transition.subtype = CATransitionSubtype.fromLeft
+        self.view.window!.layer.add(transition, forKey: nil)
+        self.dismiss(animated: false, completion: nil)
+    }
+    
+    func keyboardHandling(_ sender:UITextField) {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    // 키보드 보여주고, 화면을 올려준다
+    @objc func keyboardWillShow(_ sender: Notification)
+    {
+        NSLog("===show===")
+        self.view.frame.origin.y = -150
+    }
+    // 키보드 숨기고, 화면을 원상태로
+    @objc func keyboardWillHide(_ sender: Notification){
+        NSLog("===hids===")
+        self.view.frame.origin.y = 0
+    }
+    
+    // 엔터로 키보드 숨기기
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        NSLog("==return==")
+        textField.resignFirstResponder()
+        return true
+    }
+    //여백 클릭 시 키보드 숨기기
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    // 이미지 둥글게 하는것
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        studyImg?.contentMode = UIView.ContentMode.scaleAspectFill
+        studyImg?.layer.cornerRadius = 20.0
+        studyImg?.layer.masksToBounds = true
+    }
+    
+}
