f3035624236e00283000747c6ae11e89ac60f8eb
jybobo <jybobo@naver.com>
2019-08-27 12:52:45 +0900

    중간커밋

--- shinhanAdmin/view/registerNotice.js
+++ shinhanAdmin/view/registerNotice.js
@@ -13,7 +13,9 @@ $(document).ready(function () {
     /** start of components ***********************/
     $('#releaseYn').selectpicker();
     $('#regDate').text(moment().format('YYYY-MM-DD'));
-    //$('#date').datepicker();
+    var userInfo = JSON.parse(window.sessionStorage.getItem('userInfo'));
+    $('#writor').text(userInfo['empNm']);
+
     
     $('#daterange').daterangepicker({
         opens: 'right',
@@ -31,10 +33,6 @@ $(document).ready(function () {
         e.preventDefault();
         window.location.href = "/view/manageNotice.html";
      
-    });
-
-    $.datepicker.setDefaults({
-        dateFormat: 'yy-mm-dd' //Input Display Format 변경
     });
     
     
@@ -62,96 +60,6 @@ $(document).ready(function () {
         }
     });
     /** end of components *************************/
-    rowDoubleClick: function(args) {
-        var arr = $('#grid').jsGrid('option', 'data');
-        
-        fnGo('/view/updateContent.html', {
-            'searchReleaseYn' : $('#searchReleaseYn').val(),
-            'searchCategory' : $('#searchCategory').val(),
-            'searhRelatedTag': $('#searhRelatedTag').val(),
-            'searchTitle': $('#searchTitle').val(),
-            'rowKey': arr[args.itemIndex]['rowKey']
-        });
-    },
-
-    fields: [
-        {
-            itemTemplate: function(_, item) {
-                return $("<input>").attr("type", "checkbox")
-                        .addClass('selectionCheckbox')
-                        .prop("checked", $.inArray(item, selectedItems) > -1)
-                        .on("change", function () {
-                            $(this).is(":checked") ? selectItem(item) : unselectItem(item);
-                        });
-            },
-            align: "center",
-            width: 30
-        },
-        { name: "thumbnail", title: '썸네일', width: 80, editing: false, align: "center", cellRenderer: function(item, value) {
-            var rslt = $("<td>").addClass("jsgrid-cell");
-            var img = $('<img/>');
-            
-            $(img).css('width', '60px');
-            $(img).css('height', '45px');
-            $(img).attr('src', item);
-            $(rslt).append(img);
-            
-            return rslt;
-        }, editTemplate: function(item, value) {
-            
-            var img = $('<img/>');
-            
-            $(img).css('width', '60px');
-            $(img).css('height', '45px');
-            $(img).attr('src', item);
-            
-            return img;
-        } },
-        { name: "title", title: '컨텐츠명', type: "text", width: 150, editing: false, align: "left" },
-        { name: "author", title: '강사명', type: "text", width: 120, editing: false, align: "left" },
-        { name: "category", title: "카테고리", type: 'text', width: 200, editing: false, align: "left" },
-        { name: "tags", title: "관련태그", type: 'text', width: 200, editing: false, align: "left", cellRenderer: function(item, value){
-            var rslt = $("<td>").addClass("jsgrid-cell");
-            var div = $('<div></div>');
-            $(rslt).append(div);
-
-            if(isEmpty(item)) {
-                return rslt;
-            }
-
-            var arr = item.split(' ');
-            for(var i=0; i<arr.length; i++) {
-                $(div).append($('<span class="tag label label-info" style="margin-right:5px; display:inline-block;">'+arr[i]+'</span>'));
-            }
-            return rslt; 
-          }, editTemplate: function(item, value) {
-            var div = $('<div></div>');
-            
-            if(isEmpty(item)) {
-                return div;
-            }
-            
-            var arr = item.split(' ');
-            
-            for(var i=0; i<arr.length; i++) {
-                $(div).append($('<span class="tag label label-info" style="margin-right:5px; display:inline-block;">'+arr[i]+'</span>'));
-            }
-
-            return div; 
-        } },
-
-        //{ name: "description", title: "설명", type: 'textarea', align: "left", width: 200 },
-        { name: "releaseYn", title: "공개여부", type: 'select', items: [
-            { Name: "전체", Id: "" },
-            { Name: "Y", Id: "Y" },
-            { Name: "N", Id: "N" }
-        ],valueField: "Id", textField: "Name", width: 100, editing: true, validate: {
-            validator: 'required', 
-            message: '공개여부 는 필수입력 입니다.'
-        }, align: "center" },
-        { type: "control" } //edit control
-    ]
-});
 
 
     //validataion
@@ -164,16 +72,16 @@ $(document).ready(function () {
             param = ' 제목';
             target = $('#title');
         }
-        else if(isEmpty($('#daterange').val())) {
+        else if(isEmpty(($('#daterange > span').text()))) {
             param = '게시기간';
             target = $('#daterange');
         }
         else if(isEmpty($('#releaseYn').val())) {
             param = '공개여부';
             target = $('#releaseYn');
-        }else if($('#description').tagsinput('items').length == 0) {
+        }else if(isEmpty($('#description').val())) {
             param = '내용';
-            target = $('description');
+            target = $('#description');
         }
 
         if(!isEmpty(param)) {
@@ -191,8 +99,8 @@ $(document).ready(function () {
 
         var contentWritor = $('#writor').text();
         var contentTitle=$('#title').val();
-        var dateRangeFrom = $('#daterange').data('daterangepicker').startDate;
-        var dateRangeTo = $('#daterange').data('daterangepicker').endDate;
+        var dateRangeFrom = $('#daterange').data('daterangepicker').getStartDate();
+        var dateRangeTo = $('#daterange').data('daterangepicker').getEndDate();
         var releaseYn = $('#releaseYn').val();
         var contentDescription = $('#description').val();
                    
@@ -209,6 +117,21 @@ $(document).ready(function () {
 
 
     function setNotieDatabase(paramObj, callback) {
+        
+        console.log({
+        
+            writor: paramObj['writor'],
+            date: paramObj['date'],
+            title: paramObj['title'],
+            postingPeriodFrom: paramObj['postingPeriodFrom'],
+            postingPeriodTo: paramObj['postingPeriodTo'],
+            releaseYn: paramObj['releaseYn'],
+            description: paramObj['description']
+           
+        });
+
+        return;
+        
         //var row
         var rowKey = 'notie_' + moment().unix(); 
         firebase.database().ref('/'+ '신한은행'+'/notie/' + rowKey).set({
@@ -216,8 +139,8 @@ $(document).ready(function () {
             writor: paramObj['writor'],
             date: paramObj['date'],
             title: paramObj['title'],
-            postingPeriodFrom: dateRangeFrom,
-            postingPeriodTo: dateRangeTo,
+            postingPeriodFrom: paramObj['postingPeriodFrom'],
+            postingPeriodTo: paramObj['postingPeriodTo'],
             releaseYn: paramObj['releaseYn'],
             description: paramObj['description']
            
