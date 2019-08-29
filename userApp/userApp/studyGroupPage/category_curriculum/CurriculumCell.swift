e3328940921674b050c7c35745bbe3f05ef5be55
hs-kan <hskang2107@gmail.com>
2019-08-29 17:02:10 +0900

    CoP files added

---
+++ userApp/userApp/studyGroupPage/category_curriculum/CurriculumCell.swift
@@ -0,0 +1,19 @@
+//
+//  CurriculumCell.swift
+//  userApp
+//
+//  Created by 강희승 on 28/08/2019.
+//  Copyright © 2019 sfo. All rights reserved.
+//
+
+import UIKit
+class CurriculumCell: UICollectionViewCell {
+    @IBOutlet weak var video_img: UIImageView!
+    @IBOutlet weak var video_title: UILabel!
+    @IBOutlet weak var video_author: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
---
+++ userApp/userApp/studyGroupPage/category_member/MemberCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemberCell.swift
+//  userApp
+//
+//  Created by 강희승 on 29/08/2019.
+//  Copyright © 2019 sfo. All rights reserved.
+//
+
+import UIKit
+
+class MemberCell: UICollectionViewCell {
+    
+    @IBOutlet weak var depart_txt: UILabel!
+    @IBOutlet weak var member_txt: UILabel!
+}
