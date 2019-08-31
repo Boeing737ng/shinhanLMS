c5b5d9f054aa3db8aeae9080daf2e427be3ff80c
jybobo <jybobo@naver.com>
2019-08-31 18:33:28 +0900

    중간커밋

--- shinhanAdmin/view/manageDash.js
+++ shinhanAdmin/view/manageDash.js
@@ -5,10 +5,21 @@ $(document).ready(function () {
   var userInfo = JSON.parse(window.sessionStorage.getItem('userInfo'));
   var compCd = userInfo['compCd'];
 
+  /** start of component ***********************/
+  $('#btnMoreStudy').on('click', function (e) {
+    e.preventDefault();
+    window.parent.$('#accordionSidebar').find('a[data-url="view/manageStudy.html"]').trigger('click');
+  });
+
+
+  $('#btnMoreBbs').on('click', function (e) {
+    window.parent.$('#accordionSidebar').find('a[data-url="view/manageNotice.html"]').trigger('click');
+  });
+  /** end of component *************************/
+
 
   /** start of grid ***********************/
   /*스터디관리*/
-
   $("#grid1").jsGrid({
     width: "100%",
     height: "241px",
@@ -31,49 +42,6 @@ $(document).ready(function () {
 
   });
 
-  fnRetrieve1();
-
-
-  /*조회->스터디*/
-  function fnRetrieve1() {
-    window.FakeLoader.showOverlay();
-
-    var searchStudy = $('#studyname').val() || '';//스터디명
-    var searchDate = $('#date').val() || '';//등록일자
-    var searchCreator = $('#creator').val() || '';//참여인원
-
-
-    //searchCompany = searchCompany.toLowerCase();
-
-    parent.database.ref('/58' +'/study').once('value').then(function (snapshot) {
-
-      var catArr = snapshot.val();
-      var rsltArr = [];
-
-      $.each(catArr, function (idx, studyObj) {
-        if (
-          ((searchStudy == '') || (studyObj['studyname'].indexOf(searchStudy) > -1))
-        ) {
-          var mbrCnt = Object.keys(studyObj['member'] || []).length;
-          studyObj['participant'] = mbrCnt;
-
-          rsltArr.push(studyObj);
-        }
-
-      });
-
-      var arr = [];
-      var length = rsltArr.length > 5 ? 5 : rsltArr.length;
-      for(var i=0; i<length; i++) {
-        arr.push(rsltArr[i]);
-      }
-
-      $("#grid1").jsGrid("option", "data", arr);
-
-      window.FakeLoader.hideOverlay();
-
-    });
-  }
 
   /*공지사항관리*/
   $("#grid2").jsGrid({
@@ -107,8 +75,51 @@ $(document).ready(function () {
     ]
 
   });
+  /** end of grid *************************/
 
-  fnRetrieve2();
+
+  /** start of function ***********************/
+  /*조회->스터디*/
+  function fnRetrieve1() {
+    window.FakeLoader.showOverlay();
+
+    var searchStudy = $('#studyname').val() || '';//스터디명
+    var searchDate = $('#date').val() || '';//등록일자
+    var searchCreator = $('#creator').val() || '';//참여인원
+
+
+    //searchCompany = searchCompany.toLowerCase();
+
+    parent.database.ref('/58' + '/study').once('value').then(function (snapshot) {
+
+      var catArr = snapshot.val();
+      var rsltArr = [];
+
+      $.each(catArr, function (idx, studyObj) {
+        if (
+          ((searchStudy == '') || (studyObj['studyname'].indexOf(searchStudy) > -1))
+        ) {
+          var mbrCnt = Object.keys(studyObj['member'] || []).length;
+          studyObj['participant'] = mbrCnt;
+
+          rsltArr.push(studyObj);
+        }
+
+      });
+
+      var arr = [];
+      var length = rsltArr.length > 5 ? 5 : rsltArr.length;
+      for (var i = 0; i < length; i++) {
+        arr.push(rsltArr[i]);
+      }
+
+      $("#grid1").jsGrid("option", "data", arr);
+
+      window.FakeLoader.hideOverlay();
+
+    });
+  }
+
 
   /*조회->공지사항*/
   function fnRetrieve2() {
@@ -118,25 +129,25 @@ $(document).ready(function () {
 
       var catArr = snapshot.val();
       var rsltArr = [];
-      
+
       $.each(catArr, function (idx, studyObj) {
         studyObj['rowKey'] = idx;
         rsltArr.push(studyObj);
       });
 
       //등록일자 기준 내림차순
-      rsltArr.sort(function(a, b) { 
+      rsltArr.sort(function (a, b) {
         var bDate = moment(b.date, 'YYYYMMDD').unix();
-        var aDate = moment(a.date, 'YYYYMMDD').unix();  
+        var aDate = moment(a.date, 'YYYYMMDD').unix();
         return bDate - aDate;
       });
 
       var arr = [];
       var length = rsltArr.length > 5 ? 5 : rsltArr.length;
-      for(var i=0; i<length; i++) {
+      for (var i = 0; i < length; i++) {
         arr.push(rsltArr[i]);
       }
- 
+
       $("#grid2").jsGrid("option", "data", arr);
       window.FakeLoader.hideOverlay();
 
@@ -160,33 +171,15 @@ $(document).ready(function () {
     $('body').append(form);
     $(form).submit();
   }
+  /** end of function *************************/
 
 
-  $('#btnMoreStudy').on('click', function(e) {
-    e.preventDefault();
-
-    alert('s');
-    var sidebar = window.parent.document.getElementById('accordionSidebar');
-    $(sidebar).find('li.nav-item > a[data-url="view/manageStudy.html"]').click();
-
-        //메뉴 클릭 시
-        /* $(document).on('click', '#accordionSidebar li.nav-item > a', function (e) {
-          var title = $(this).find('span').text();
-          var url = $(this).attr('data-url');
-          fnLoadPage(title, url);
-  
-          $('#accordionSidebar li.nav-item').removeClass('active');
-          $(this).parent('li').addClass('active');
-      }); */
-  });
 
 
-  $('#btnMoreBbs').on('click', function(e) {
 
-  });
+
 
   /*************************차트***************************/
-
   // Set new default font family and font color to mimic Bootstrap's default styling
   Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
   Chart.defaults.global.defaultFontColor = '#858796';
@@ -219,13 +212,13 @@ $(document).ready(function () {
 
 
 
-/***************차트표현하는 함수**************** */
+  /***************차트표현하는 함수**************** */
 
   function fnRetrieve3() {
-      var view=[];
-      var title=[];
+    var view = [];
+    var title = [];
 
-    
+
     parent.database.ref('/' + compCd + '/videos').once('value').then(function (snapshot) {
       var catArr = snapshot.val();
       var chartArr = [];
@@ -237,111 +230,107 @@ $(document).ready(function () {
         view.push(chartObj['view']); //조회수정보
         title.push(chartObj['title']) //동영상조회수
 
-      }); 
-     // console.log('정렬전:'+view);
+      });
+      // console.log('정렬전:'+view);
 
-      function compare ( a , b ) 
-        {   
-          return  b-a;   
-        } 
+      function compare(a, b) {
+        return b - a;
+      }
 
       view.sort(compare);
 
-      //console.log('정렬후:'+view);
 
-
-  
-
-  var ctx = document.getElementById("view_count_chart");
-  var myBarChart = new Chart(ctx, {
-    type: 'bar',
-      data: {
-        labels: ["1위", "2위", "3위", "4위", "5위", "6위", "7위", "8위", "9위", "10위"],
-        datasets: [{
-          label: "조회수 : ",
-          backgroundColor: "#4e73df",
-          hoverBackgroundColor: "#2e59d9",
-          borderColor: "#4e73df",
-          data: view
-        }],
-      },
-    options: {
-      maintainAspectRatio: false,
-            layout: {
-                padding: {
-                  left: 10,
-                  right: 25,
-                  top: 25,
-                  bottom: 0
-              }
-            },
-            scales: {
-                xAxes: [{
-                  time: {
-                    unit: 'month'
-                  },
-                  gridLines: {
-                    display: false,
-                    drawBorder: false
-                  },
-                  ticks: {
-                    maxTicksLimit: 10
-                  },
-                  maxBarThickness: 25,
-                }],
-                yAxes: [{
-                  ticks: {
-                    min: 0,
-                    max: 30000,
-                    maxTicksLimit: 10,
-                    padding: 10,
-                    callback: function (value, index, values) {
-                      return number_format(value) + ' 회'; //y축 단위 표시 
-                    }
-                  },
-                  gridLines: {
-                    color: "rgb(234, 236, 244)",
-                    zeroLineColor: "rgb(234, 236, 244)",
-                    drawBorder: false,
-                    borderDash: [2],
-                    zeroLineBorderDash: [2]
-                  }          
-                }],
+      var ctx = document.getElementById("view_count_chart");
+      var myBarChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ["1위", "2위", "3위", "4위", "5위", "6위", "7위", "8위", "9위", "10위"],
+          datasets: [{
+            label: "조회수 : ",
+            backgroundColor: "#4e73df",
+            hoverBackgroundColor: "#2e59d9",
+            borderColor: "#4e73df",
+            data: view
+          }],
+        },
+        options: {
+          maintainAspectRatio: false,
+          layout: {
+            padding: {
+              left: 10,
+              right: 25,
+              top: 25,
+              bottom: 0
+            }
+          },
+          scales: {
+            xAxes: [{
+              time: {
+                unit: 'month'
               },
-                  legend: {
-                    display: false
-                  },
-      /** 마우스 가져다 대면 출력**/
-      tooltips: {
-        titleMarginBottom: 10,
-        titleFontColor: '#6e707e',
-        titleFontSize: 14,
-        backgroundColor: "rgb(255,255,255)",
-        bodyFontColor: "#858796",
-        borderColor: '#dddfeb',
-        borderWidth: 1,
-        xPadding: 15,
-        yPadding: 15,
-        displayColors: false,
-        caretPadding: 10,
-        callbacks: {
-          label: function (tooltipItem, chart) {
-            var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-            return datasetLabel + number_format(tooltipItem.yLabel) + ' 회';
-          }
+              gridLines: {
+                display: false,
+                drawBorder: false
+              },
+              ticks: {
+                maxTicksLimit: 10
+              },
+              maxBarThickness: 25,
+            }],
+            yAxes: [{
+              ticks: {
+                min: 0,
+                max: 30000,
+                maxTicksLimit: 10,
+                padding: 10,
+                callback: function (value, index, values) {
+                  return number_format(value) + ' 회'; //y축 단위 표시 
+                }
+              },
+              gridLines: {
+                color: "rgb(234, 236, 244)",
+                zeroLineColor: "rgb(234, 236, 244)",
+                drawBorder: false,
+                borderDash: [2],
+                zeroLineBorderDash: [2]
+              }
+            }],
+          },
+          legend: {
+            display: false
+          },
+          /** 마우스 가져다 대면 출력**/
+          tooltips: {
+            titleMarginBottom: 10,
+            titleFontColor: '#6e707e',
+            titleFontSize: 14,
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+            callbacks: {
+              label: function (tooltipItem, chart) {
+                var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                return datasetLabel + number_format(tooltipItem.yLabel) + ' 회';
+              }
+            }
+          },
         }
-      },
-    }
-  });
+      });
 
 
-});
+    });
   }
+
+
+  fnRetrieve1();
+  fnRetrieve2();
   fnRetrieve3();
   //resize frame height
   resizeFrame();
-
-
-
 
 });
