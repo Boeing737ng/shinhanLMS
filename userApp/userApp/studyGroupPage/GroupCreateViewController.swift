5f14318f01f9e1f328731f9bcb6ef68c7fbcff16
hs-kan <hskang2107@gmail.com>
2019-09-05 02:34:00 +0900

    study

--- userApp/userApp/studyGroupPage/GroupCreateViewController.swift
+++ userApp/userApp/studyGroupPage/GroupCreateViewController.swift
@@ -88,7 +88,9 @@ class GroupCreateViewController: UIViewController, UIImagePickerControllerDelega
         transition.subtype = CATransitionSubtype.fromLeft
         self.view.window!.layer.add(transition, forKey: nil)
         self.dismiss(animated: false, completion: nil)
+        curri_send = key
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: "addcop"), object: nil)
+        
     }
     
     func openLibrary(){
--- userApp/userApp/studyGroupPage/category_member/GroupMember.swift
+++ userApp/userApp/studyGroupPage/category_member/GroupMember.swift
@@ -11,48 +11,21 @@ class GroupMember: UICollectionView, UICollectionViewDelegate, UICollectionViewD
     override func awakeFromNib() {
         self.delegate = self
         self.dataSource = self
-        getData()
-        NotificationCenter.default.addObserver(self, selector: #selector(reloadcollection), name: NSNotification.Name(rawValue: "copchange"), object: nil)
-        
+        NotificationCenter.default.addObserver(self, selector: #selector(reloadMember), name: NSNotification.Name(rawValue: "copchange"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(reloadMember), name: NSNotification.Name(rawValue: "memberAdd"), object: nil)
     }
     
     @objc func reloadMember() {
-        getData()
-        self.reloadData()
+        if(curri_send == "0"){
+            initArrays()
+            self.reloadData()
+        }
+        else {
+            getData()
+            self.reloadData()
+        }
+        
     }
-    
-    @objc func reloadcollection() {
-        getData()
-        self.reloadData()
-    }
-    
-//    func initmember()
-//    {
-//        initArrays()
-//        var index = 0
-//        var ref: DatabaseReference!
-//        ref = Database.database().reference()
-//        ref.child("58/study/11111/member").observeSingleEvent(of: .value, with: { (snapshot) in
-//            if snapshot.childrenCount == 0 {
-//                return
-//            }
-//            let value = snapshot.value as? Dictionary<String,Any>;()
-//            for study in value! {
-//                let studyDict = study.value as! Dictionary<String, Any>;()
-//                let name = studyDict["name"] as! String
-//                let depart = studyDict["department"] as! String
-//                self.member_name.append(name)
-//                self.member_depart.append(depart)
-//                index += 1
-//            }
-//            self.dataReceived = true
-//            self.reloadData()
-//        }) { (error) in
-//            print(error.localizedDescription)
-//        }
-//    }
-    
     func getData()
     {
         initArrays()
@@ -70,7 +43,6 @@ class GroupMember: UICollectionView, UICollectionViewDelegate, UICollectionViewD
                 let depart = studyDict["department"] as! String
                 self.member_name.append(name)
                 self.member_depart.append(depart)
-                //index += 1
             }
             self.dataReceived = true
             self.reloadData()
@@ -78,21 +50,12 @@ class GroupMember: UICollectionView, UICollectionViewDelegate, UICollectionViewD
             print(error.localizedDescription)
         }
     }
-    
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return member_name.count
     }
-    
-    //    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    //        let videoId = recentVideoIdArray[indexPath.row]
-    //        selectedVideoId = videoId
-    //        TabViewController().goToDetailPage()
-    //    }
-    
     func initArrays() {
         member_depart.removeAll()
         member_name.removeAll()
@@ -108,21 +71,11 @@ class GroupMember: UICollectionView, UICollectionViewDelegate, UICollectionViewD
         if dataReceived {
             cell.depart_txt.text = member_depart[indexPath.row]
             cell.member_txt.text = member_name[indexPath.row]
-            //cell.video_img.image = CachedImageView().loadCacheImage(urlKey: recentVideoIdArray[indexPath.row])
         } else {
             cell.depart_txt.text = textArray[indexPath.row]
             cell.member_txt.text = authorArray[indexPath.row]
-            //cell.video_img.image = UIImage(named: "white.jpg")
         }
         
         return cell
     }
-    /*
-     // Only override draw() if you perform custom drawing.
-     // An empty implementation adversely affects performance during animation.
-     override func draw(_ rect: CGRect) {
-     // Drawing code
-     }
-     */
-    
 }
