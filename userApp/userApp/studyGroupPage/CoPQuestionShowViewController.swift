490b0345ef4f69e877847795882ac234e3157d11
Da-inKim <ll09072@naver.com>
2019-09-04 14:54:41 +0900

    CoP Post Show page keyboard issue modified.

--- userApp/userApp/studyGroupPage/CoPQuestionShowViewController.swift
+++ userApp/userApp/studyGroupPage/CoPQuestionShowViewController.swift
@@ -21,7 +21,10 @@ class CoPQuestionShowViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var btnSubmit: UIButton!
     @IBOutlet weak var commentTableView: UIView!
     
+    
     var keyboardSettingHeight: CGFloat = 0
+    var originalTf: CGFloat = 0
+    var originalBtn: CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -92,8 +95,6 @@ class CoPQuestionShowViewController: UIViewController, UITextFieldDelegate {
         // 키보드가 사라질때~
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)),name:UIResponder.keyboardWillHideNotification, object: nil)
         
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow(_:)),name:UIResponder.keyboardDidShowNotification, object: nil)
-        
     }
     
     // 키보드 보이기
@@ -104,12 +105,15 @@ class CoPQuestionShowViewController: UIViewController, UITextFieldDelegate {
             let keyboardRectangle = keyboardFrame.cgRectValue
             let keyboardHeight = keyboardRectangle.height
             
+            originalTf = self.tfComment.frame.origin.y
+            originalBtn = self.btnSubmit.frame.origin.y
             keyboardSettingHeight = self.view.frame.height - keyboardHeight - tfComment.frame.height - 5
             
-            print(self.view.frame.height)
-            print(keyboardHeight)
+            //print(self.view.frame.height)
+            //print(keyboardHeight)
             
-            //self.commentView.frame.origin.y = self.view.frame.height - keyboardHeight - commentView.frame.height - 5
+            self.tfComment.translatesAutoresizingMaskIntoConstraints = true
+            self.btnSubmit.translatesAutoresizingMaskIntoConstraints = true
             self.tfComment.frame.origin.y = keyboardSettingHeight
             self.btnSubmit.frame.origin.y = keyboardSettingHeight
         }
@@ -118,16 +122,8 @@ class CoPQuestionShowViewController: UIViewController, UITextFieldDelegate {
     // 키보드 숨기기
     @objc func keyboardWillHide(_ sender: Notification) {
         NSLog("===== keyboardWillHide =====")
-        
-        self.tfComment.frame.origin.y = 0
-        self.btnSubmit.frame.origin.y = 0
-    }
-    
-    @objc func keyboardDidShow(_ sender: Notification) {
-        NSLog("===== keyboardDidShow =====")
-        
-        self.tfComment.frame.origin.y = keyboardSettingHeight
-        self.btnSubmit.frame.origin.y = keyboardSettingHeight
+        self.tfComment.frame.origin.y = originalTf
+        self.btnSubmit.frame.origin.y = originalBtn
     }
     
     // 엔터로 키보드 내리기
@@ -140,58 +136,7 @@ class CoPQuestionShowViewController: UIViewController, UITextFieldDelegate {
     
     // 아무데나 선택 시 키보드 내리기
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
         self.view.endEditing(true)
     }
-    
-    //    @objc func keyboardWillShow(_ sender: Notification) {
-    //        NSLog("====== keyboardWillShow ======")
-    //
-    //        if let keyboardFrame: NSValue = sender.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
-    //            let keyboardRectangle = keyboardFrame.cgRectValue
-    //            let keyboardHeight = keyboardRectangle.height
-    //
-    //            keyboardSettingHeight = self.view.frame.height - keyboardHeight - tfComment.frame.height - 5
-    //
-    //            print(self.view.frame.height)
-    //            print(keyboardHeight)
-    //
-    //            //self.commentView.frame.origin.y = self.view.frame.height - keyboardHeight - commentView.frame.height - 5
-    //            self.tfComment.frame.origin.y = keyboardSettingHeight
-    //            self.btnSubmit.frame.origin.y = keyboardSettingHeight
-    //        }
-    //    }
-    //
-    //    @objc func keyboardWillHide(_ sender: Notification) {
-    //        NSLog("====== keyboardWillHide ======")
-    ////        self.commentView.frame.origin.y = 0
-    //    }
-    //
-    //    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-    //        NSLog("====== textFieldShouldReturn ======")
-    //        textField.resignFirstResponder()
-    //        return true
-    //    }
-    //
-    //    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-    //        print("here")
-    //        self.tfComment.frame.origin.y = keyboardSettingHeight
-    //        self.btnSubmit.frame.origin.y = keyboardSettingHeight
-    //
-    //        return true
-    //    }
-    //
-    //    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    //        self.view.endEditing(true)
-    //    }
-    //
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
